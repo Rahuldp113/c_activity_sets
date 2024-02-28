969abe7e1fd66c927de8735ec4c4ecae8725ae06
Rahuldp113 <rahul18abd@gmail.com>
2024-02-29 01:06:36 +0530

    problem01.c

---
+++ set02/problem01.c
@@ -0,0 +1,5 @@
+//01.  Write a program to find the area of a triangle.
+
+void input(float *base, float *height);
+void find_area(float base , float height, float *area);
+void output(float base, float height, float area);
